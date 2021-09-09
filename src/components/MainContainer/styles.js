34ac9b435844be84a1455f175c7c62a52f409065
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-09-08 22:45:59 -0300

    remove files and create components

---
+++ src/components/MainContainer/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+    background: #14022C;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
