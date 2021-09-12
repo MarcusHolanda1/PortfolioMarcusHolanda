c5774c081b660b002fe9c840d9c61578b1ef026b
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-09-12 00:08:07 -0300

    Add Typewriter

--- src/components/titlesProfile/index.jsx
+++ src/components/titlesProfile/index.jsx
@@ -1,14 +1,28 @@
-import {TitlesDiv} from './styles'
+import { TitlesDiv, MyName } from "./styles";
+import Typewriter from "typewriter-effect";
 
-
-
-const TitlesProfile = props => {
+const TitlesProfile = (props) => {
     return (
         <TitlesDiv>
-            
+            <MyName>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                        .start()
+                        .typeString("Olá, tudo bem?") 
+                        .pauseFor(5000)
+                        .deleteAll()
+                        .typeString("Meu nome é Marcus Holanda")
+                        .pauseFor(2000)
+                        .deleteAll()
+                        .typeString("Tenho 20 anos")
+                        .pauseFor(2000)
+                        .deleteAll()
+                        .typeString("Sou Desenvolvedor Front-End")
+                    }}
+                ></Typewriter>
+            </MyName>
         </TitlesDiv>
-    )
-
-
-}
+    );
+};
 export default TitlesProfile;
--- src/components/titlesProfile/styles.js
+++ src/components/titlesProfile/styles.js
@@ -4,16 +4,16 @@ export const TitlesDiv = styled.div`
     position: absolute;
     top: 0;
     left: 250px;
-    width: 600px;
+    width: 630px;
     height: 245px;
-    border: 5px solid red; 
+ 
 `
 
 export const MyName = styled.h1`
     font-style: normal;
-font-weight: normal;
-font-size: 36px;
-line-height: 42px;
+    font-weight: normal;
+    margin-top: 20px;
+    font-size: 40px;
 
-color: #FFFFFF;
+    color: #FFFFFF;
 `
