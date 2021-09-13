30a4f9d757ca4df8e89f9d2a1dc819362ed4d329
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-09-13 11:26:28 -0300

    Other card

--- src/components/experienceProfessional/index.jsx
+++ src/components/experienceProfessional/index.jsx
@@ -7,34 +7,60 @@ import {
     ExpAPP,
     ExpText,
     DivExp,
+    TitleExperienceProfessionalTester,
+    CardExperienceProfessionalTester,
+    ImageCardTester,
+    DivExpTester,
+    TitleCardTester,
+    ExpAPPTester,
+    ExpTextTester,
 } from "./styles";
 
 import DevImage from "../../assets/images/programador.png";
+import TesterImage from "../../assets/images/bug-do-computador.png";
 
 const ExperienceProfessional = (props) => {
     return (
-        <DivExperienceProfessional>
+        <>
             <TitleExperienceProfessional>
                 Experiência profissional
             </TitleExperienceProfessional>
-            <CardExperienceProfessional>
-                <ImageCard src={DevImage}></ImageCard>
-                <DivExp>
-                    <TitleCard>Desenvolvedor Mobile</TitleCard>
-                    <ExpAPP>
-                        PaP | Aplicativo de mobilidade | Junho de 2021 -
-                        Atualmente
-                    </ExpAPP>
-                    <ExpText>
-                        Desenvolvimento em dois aplicativos de mobilidade (IOS e
-                        Android) cujo um app é voltado para o passageiro e outro
-                        para o motorista, o aplicativo possui as plataformas de
-                        transporte terrestre, náutico e aéreo
-                    </ExpText>
+            <DivExperienceProfessional>
+                <CardExperienceProfessional>
+                    <ImageCard src={DevImage}></ImageCard>
+                    <DivExp>
+                        <TitleCard>Desenvolvedor Mobile</TitleCard>
+                        <ExpAPP>
+                            PaP | Aplicativo de mobilidade | Junho de 2021 -
+                            Atualmente
+                        </ExpAPP>
+                        <ExpText>
+                            Desenvolvimento em dois aplicativos de mobilidade
+                            (IOS e Android) cujo um app é voltado para o
+                            passageiro e outro para o motorista, o aplicativo
+                            possui as plataformas de transporte terrestre,
+                            náutico e aéreo
+                        </ExpText>
+                    </DivExp>
+                </CardExperienceProfessional>
 
-                </DivExp>
-            </CardExperienceProfessional>
-        </DivExperienceProfessional>
+                <CardExperienceProfessionalTester>
+                    <ImageCardTester src={TesterImage}></ImageCardTester>
+                    <DivExpTester>
+                        <TitleCardTester>QA Tester</TitleCardTester>
+                        <ExpAPPTester>
+                            PaP | Aplicativo de mobilidade | Março de 2021 -
+                            Junho de 2021
+                        </ExpAPPTester>
+                        <ExpTextTester>
+                            Qa tester na PaP adquirindo todo conhecimento e
+                            experiência que um responsável pela qualidade da
+                            aplicação deve ter.
+                        </ExpTextTester>
+                    </DivExpTester>
+                </CardExperienceProfessionalTester>
+            </DivExperienceProfessional>
+        </>
     );
 };
 
--- src/components/experienceProfessional/styles.js
+++ src/components/experienceProfessional/styles.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const DivExperienceProfessional = styled.div`
-    position: absolute;
     top: 0;
     width: 900px;
-    height: 230px;
+    height: 200px;
     margin-left: 0px;
     margin-top: 10px;
+
+    display: flex;
+    flex-direction: row;
 `;
 
 export const TitleExperienceProfessional = styled.h2`
@@ -84,3 +86,81 @@ export const ExpText = styled.h4`
     font-weight: normal;
 `
 
+///Tester
+
+
+
+export const TitleExperienceProfessionalTester = styled.h2`
+    font-weight: 300;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    padding-bottom: 5px;
+    position: relative;
+
+    color: white;
+
+    &:after {
+        content: "";
+        position: absolute;
+        width: 20%;
+        height: 1px;
+        bottom: 0;
+        border-bottom: 1px solid #7806bd;
+    }
+`;
+
+export const CardExperienceProfessionalTester = styled.div`
+    margin-top: 20px;
+    margin-left: 115px;
+    width: 340px;
+    height: 340px;
+    display: inline-block;
+    flex-direction: row;
+    background: #8728ff;
+    border-radius: 10px;
+`;
+
+export const ImageCardTester = styled.img`
+    width: 97px;
+    height: 97px;
+
+    display: block;
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const DivExpTester = styled.div`
+    width: 280px;
+    height: 240px;
+
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const TitleCardTester = styled.h3`
+    margin-top: 20px;
+    color: white;
+    font-size: 18px;
+    text-align: center;
+`;
+export const ExpAPPTester = styled.h4`
+    font-style: normal;
+    font-weight: 300;
+    font-size: 11px;
+    
+    color: #b6b6b6;
+
+    margin-top: 20px;
+`;
+
+export const ExpTextTester = styled.h4`
+    margin-top: 10px;
+    font-family: Roboto, sans-serif;
+    font-size: 14px;
+
+    color: white;
+    font-weight: normal;
+`
