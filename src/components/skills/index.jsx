9ef8c22f5f77fb6277a83d18c034735d53e6f2b9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-09-13 21:14:29 -0300

    Other table done

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -11,6 +11,11 @@ import ImageSkillsReactJS from "../../assets/icons/react.svg";
 import ImageSkillsNative from "../../assets/icons/icons8-nativo-de-reagir-512 (1) 1.svg";
 import ImageSkillsRuby from "../../assets/icons/ruby3.svg";
 
+import ImageSkillsGit from '../../assets/icons/git.svg'
+import ImageSkillsMySQL from '../../assets/icons/mysql.svg'
+import ImageSkillsSwagger from '../../assets/icons/swagger.svg'
+import ImageSkillsFigma from '../../assets/icons/figma.svg'
+
 const Skills = (props) => {
     return (
         <>
@@ -38,20 +43,20 @@ const Skills = (props) => {
         <DivSkills>
             <SkillsTitle>Outras habilidades</SkillsTitle>
             <DivSkillTest>
-                <ImageSkillsTest src={ImageSkillsJavaScript} width={'32px'} height={'32px'}></ImageSkillsTest>
-                <SpanSkillTest>JavaScript</SpanSkillTest>
+                <ImageSkillsTest src={ImageSkillsGit} width={'32px'} height={'32px'}></ImageSkillsTest>
+                <SpanSkillTest>Git / Gitflow</SpanSkillTest>
             </DivSkillTest>
             <DivSkillTest>
-                <ImageSkillsTest src={ImageSkillsReactJS} width={'35px'} height={'35px'}></ImageSkillsTest>
-                <SpanSkillTest>React JS</SpanSkillTest>
+                <ImageSkillsTest src={ImageSkillsMySQL} width={'35px'} height={'35px'}></ImageSkillsTest>
+                <SpanSkillTest>MySQL</SpanSkillTest>
             </DivSkillTest>
             <DivSkillTest>
-                <ImageSkillsTest src={ImageSkillsNative} width={'35px'} height={'38px'}></ImageSkillsTest>
-                <SpanSkillTest>React Native</SpanSkillTest>
+                <ImageSkillsTest src={ImageSkillsSwagger} width={'35px'} height={'38px'}></ImageSkillsTest>
+                <SpanSkillTest>Swagget API</SpanSkillTest>
             </DivSkillTest>
             <DivSkillTest>
-                <ImageSkillsTest src={ImageSkillsRuby} width={'30px'} height={'37px'}></ImageSkillsTest>
-                <SpanSkillTest>Ruby on Rails</SpanSkillTest>
+                <ImageSkillsTest src={ImageSkillsFigma} width={'30px'} height={'37px'}></ImageSkillsTest>
+                <SpanSkillTest>Figma</SpanSkillTest>
             </DivSkillTest>
         </DivSkills>
         </>
